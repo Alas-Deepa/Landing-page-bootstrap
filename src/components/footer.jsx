5571943ca93ae4bf603f7fac4483f234cb37d194
Alas-Deepa <162786929+Alas-Deepa@users.noreply.github.com>
2024-03-16 14:12:54 +0530

    landing page with bootstrap

---
+++ src/components/footer.jsx
@@ -0,0 +1,14 @@
+export const Footer = (props) => {
+  
+  return (
+    <div>
+      <div id='footer'>
+        <div className='container text-center'>
+          <p>
+           Copyright &copy; 2024. All rights reserved.            
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
